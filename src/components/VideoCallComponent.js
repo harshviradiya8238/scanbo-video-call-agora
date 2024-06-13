4beb3cac1c4736cb2a71f56f38bfa1bd634b7fec
harsh viradiya <harshviradiya2000@gmail.com>
2024-06-13 12:02:10 +0530

    --update tag video true

--- src/components/VideoCallComponent.js
+++ src/components/VideoCallComponent.js
@@ -29,7 +29,7 @@ function VideoCallComponent({ setVideoCallConfig, doctor_id }) {
         appId: '85fd7235064249f0a0997a368d7a24d6',
         channel: cname ? cname : "", // your agora channel
         // enableScreensharing: true,
-        video: false,
+        video: true,
         token: null // use null or skip if using app in testing mode
     };
 
@@ -73,7 +73,7 @@ function VideoCallComponent({ setVideoCallConfig, doctor_id }) {
                 <AgoraUIKit
                     rtcProps={rtcProps}
                     callbacks={callbacks}
-                    video={false}
+                    video={true}
                 // ...other props you need to pass to the AgoraUIKit
                 />
             )}
